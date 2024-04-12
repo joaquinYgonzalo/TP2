06de191be89f72c9412eea9eba8b5652cc106eee
joaquinYgonzalo <166860732+joaquinYgonzalo@users.noreply.github.com>
2024-04-12 17:21:17 -0300

    Add files via upload

---
+++ punto14.c
@@ -0,0 +1,15 @@
+#include "MKL25Z4.h"
+
+
+int main(){
+
+    SIM -> SCGC5 |= SIM_SCGCS_PORTA_MASK | SIMSCGC5_PORTB_MASK;
+    PORTB->PDDR|=0x00000000;
+    PORTA->PDDR|=0XFFFFFFFF;
+    
+    while(1){
+        uint32_t est_ptB=PTB->PDIR;
+        PTA-> PDOR |= est_ptB >>2;
+    }
+    return 0;
+}
