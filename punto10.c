3cc0b463eece3949f8a9e5c001d31e3e71ebaeab
joaquinYgonzalo <166860732+joaquinYgonzalo@users.noreply.github.com>
2024-04-26 14:59:31 -0300

    punto10.c

---
+++ punto10.c
@@ -0,0 +1,13 @@
+#include"MKL25Z4.h"
+
+int main(){
+    SIM -> SCGC5 |= SIM_SCGCS_PORTA_MASK | SIMSCGC5_PORTB_MASK;
+    PORTB->PDDR|=0x00000000;
+    PORTA->PDDR|=0XFFFFFFFF;
+
+while(1){
+    uint32_t  est_ptB= PTB->PDOR|=0XFFFFFFFF;
+    PTA-> PDOR |= ~(est_ptB | 0x23);
+    }
+return 0;
+}
